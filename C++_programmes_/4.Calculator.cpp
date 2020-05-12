86d8ca492eaf160d3ab44b42e0f1ab7edddcdab8
Pranav <pranavgoel29@gmail.com>
2020-05-12 06:30:04 +0530

    Added calculator programme

---
+++ C++_programmes_/4.Calculator.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    system("cls");
+    char ch;
+    float a, b, c, i;
+    cout<<"CALCULATOR!"<<endl;
+    {
+    dominator:
+    cout<<"\nEnter first value: ";
+    cin>>a;
+    cout<<"Enter the second value: ";
+    cin>>b;
+    op:
+    cout<<"Enter an operator(+, -, *, /): ";
+    cin>>ch;
+    switch(ch)
+    {
+        case '+': c = a+b;
+                  cout<<"\nThe sum of the given value is "<<c<<endl;
+                  break;
+        case '-': c = a-b;
+                  cout<<"\nThe difference between the given value is "<<c<<endl;
+                  break;
+        case '*': c = a*b;
+                  cout<<"\nThe product of the given value is "<<c<<endl;
+                  break;
+        case '/': if(b == 0)
+                    {
+                        cout<<"\nThe dominator cannot be zero."<<endl;
+                        goto dominator;
+                    }
+                  else
+                  c= a/b;
+                  cout<<"\nThe quotient of the given value is "<<c<<endl;
+                  break;
+        default: cout<<"\nYou entered an invalid operator!"<<endl;
+                 cout<<"Please choose an valid operator!\n"<<endl;
+                 goto op;
+    }
+
+    }
+    getch();
+    return 0;
+}

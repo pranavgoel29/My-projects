73e1c6fc4dfaa5f871c53e268c41f7e0b2db0bf0
pranavgoel29 <pranavgoel29@gmail.com>
2022-08-05 00:08:33 +0530

    Added a program for iterator

---
+++ C++_programmes_/8_Iterators.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<iterator>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> ar = { 1, 2, 3, 4, 5 };
+      
+    // Declaring iterator to a vector
+    vector<int>::iterator ptr;
+      
+    // Displaying vector elements using begin() and end()
+    cout << "The vector elements are : ";
+    for (ptr = ar.begin(); ptr < ar.end(); ptr++)
+        cout << *ptr << " ";
+      
+    return 0;    
+}
